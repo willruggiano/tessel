41a5859502adf15c16bc32f1f2365156273a5b09
Xavier Durand-Smet <xavier.durand-smet>
2015-08-14 16:02:25 -0400

    rfid works

---
+++ rfid/rfid.js
@@ -0,0 +1,21 @@
+// This basic RFID example listens for an RFID
+// device to come within range of the module,
+// then logs its UID to the console.
+// *********************************************/
+
+var tessel = require('tessel');
+var rfidlib = require('rfid-pn532');
+
+var rfid = rfidlib.use(tessel.port['A']); 
+
+rfid.on('ready', function (version) {
+  console.log('Ready to read RFID card');
+
+  rfid.on('data', function(card) {
+    console.log('UID:', card.uid.toString('hex'));
+  });
+});
+
+rfid.on('error', function (err) {
+  console.error(err);
+});
